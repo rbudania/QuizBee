077d81152235280587c3e2bf331bca337d5f90f2
Rajat <rbudania99@gmail.com>
2020-04-22 00:41:11 +0530

    Updates

---
+++ precache-manifest.bc797b419a83ae829d47c0fa2b36b8bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493b6439c72444576dd46ee55fb52144",
+    "url": "/QuizBee/index.html"
+  },
+  {
+    "revision": "cc5059cad6e4fda95469",
+    "url": "/QuizBee/static/css/main.cba5848a.chunk.css"
+  },
+  {
+    "revision": "fd155d557cacdc2d576a",
+    "url": "/QuizBee/static/js/2.6ec2dcca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizBee/static/js/2.6ec2dcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5059cad6e4fda95469",
+    "url": "/QuizBee/static/js/main.7952f750.chunk.js"
+  },
+  {
+    "revision": "cab08e5f80c65bb6bc20",
+    "url": "/QuizBee/static/js/runtime-main.4586933c.js"
+  }
+]);
